6d6ae20ef527973fd11644880d3bbe28d6c44a03
Harpreet <ksfibri@gmail.com>
2026-01-15 10:09:57 -0600

    Add comment Add comment

--- Space Exception/Program.cs
+++ Space Exception/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace Space_Exception
+﻿using System;
+using System.ComponentModel.Design;
+using System.Security.Cryptography;
+
+namespace Space_Exception
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+           // You are managing a gallery’s digital collection, where each piece of artwork is carefully cataloged by title,
+           // artist, year, and medium.In this assignment, you’ll create the Art Collection Manager,
+           // a console application designed to help organize and explore this collection.
+           // This project will allow you to practice essential programming skills like data handling, file I/ O,
+           // algorithm implementation, and structured planning through flowcharts and pseudocode.
         }
     }
 }
